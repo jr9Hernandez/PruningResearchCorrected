37d7580529328c65b198e90fe3d57593415aeb8c
Julian <Julian@DESKTOP-FCQ3307>
2017-01-24 14:26:20 -0200

    fue modificado la parte de podar la mitad de las telas porque es una repeticion

--- PruningMarioResearch/src/graphBuilder/GraphBuilder.java
+++ PruningMarioResearch/src/graphBuilder/GraphBuilder.java
@@ -915,7 +915,8 @@ public class GraphBuilder
     	countElements--;
     	Elements objElem= (Elements)finalList.get(countElementsFinal-countElements-1);
     	int idElem=objElem.getIdElem();
-    	int typeElem=objElem.getTypeElem(); 
+    	int typeElem=objElem.getTypeElem();
+    	int widthElemm=objElem.getWidth();
     	
     	int quadrant1X=0;
     	int quadrant2X=0;
@@ -977,19 +978,59 @@ public class GraphBuilder
     	 {       
     		 int indeyN=j;
     		 
+    		 if(minor==0)
+    		 {
+       		 	if(objElem.getIdElem()==0)
+       		 	{   			     			
+       		 		if(indexN>globalCenterXMass)
+       		 		{
+       				 
+       		 			continue;
+       		 		}
+       		 	}
+    		 }
+    		 
     		 if(minor==1)
     		 {
     			indexN= (maxRight-i+1); 
+    			
+       		 	if(objElem.getIdElem()==0)
+       		 	{   			     			
+       		 		if((indexN+widthElemm)<globalCenterXMass)
+       		 		{
+       				 
+       		 			continue;
+       		 		}
+       		 	}
     		 }
     		 else if(minor==2)
     		 {
     			 indeyN=(height-j-1)+ruleThirds; 
+    			 
+        		 if(objElem.getIdElem()==0)
+           		 {   			     			
+           		 	if(indexN>globalCenterXMass)
+           		 	{
+           				 
+           		 		continue;
+           		 	}
+           		 }
     		 }
     		 else if(minor==3)
     		 {
     			 indexN= (maxRight-i+1);
-    			 indeyN=(height-j-1)+ruleThirds; 
+    			 indeyN=(height-j-1)+ruleThirds;
+    			 
+        		 if(objElem.getIdElem()==0)
+           		 {   			     			
+        		 	if((indexN+widthElemm)<globalCenterXMass)
+           		 	{
+           				 
+           		 		continue;
+           		 	}
+           		 }
     		 }
+    		 
     		 
     		 /*if(quadrant1X>quadrant2X)
     		 {
@@ -1000,14 +1041,7 @@ public class GraphBuilder
     		 {
     			 indeyN=(height-j-1)+ruleThirds;
     		 }*/
-    		 if(objElem.getIdElem()==0)
-    		 {   			     			
-    			 if(indexN>globalCenterXMass)
-    			 {
-    				 
-    				 continue;
-    			 }
-    		 }
+
     		 
     		 if(typeElem==objElemP.getOddsCannons() || typeElem==objElemP.getOddsTubes() || typeElem==objElemP.getTubesFlower())
     		 {    			
