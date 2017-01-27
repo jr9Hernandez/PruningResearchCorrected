646bbfdafcc9cec58cc50b72a283e354c27c7cdd
Julian <Julian@DESKTOP-FCQ3307>
2017-01-27 01:45:05 -0200

    same anterior

--- PruningMarioResearch/src/graphBuilder/GraphBuilder.java
+++ PruningMarioResearch/src/graphBuilder/GraphBuilder.java
@@ -791,10 +791,10 @@ public class GraphBuilder
         	XC1=firstX+secondX+thirdX;
         	YC1=firstY+secondY+thirdY;
         	
-        	XC1=2*firstXSecondHeuristic+2*secondXSecondHeuristic+thirdXSecondHeuristic;
-        	YC1=2*firstYSecondHeuristic+2*secondYSecondHeuristic+thirdYSecondHeuristic;
+        	XC2=2*firstXSecondHeuristic+2*secondXSecondHeuristic+thirdXSecondHeuristic;
+        	YC2=2*firstYSecondHeuristic+2*secondYSecondHeuristic+thirdYSecondHeuristic;
         	
-        	partialSymmetry=partialSymmetry-((3*areaElement)+(Math.max(XC1, XC1))+(Math.max(YC1, YC1)));
+        	partialSymmetry=partialSymmetry-((3*areaElement)+(Math.max(XC1, XC2))+(Math.max(YC1, YC2)));
         	//firstheuristicCandidate=firstheuristicCandidate+((3*areaElement)+firstX+secondX+thirdX+firstY+secondY+thirdY);
         	//secondheuristicCandidate=secondheuristicCandidate+((3*areaElement)+(2*firstX)+(2*secondX)+thirdX+firstY+secondY+thirdY);
 
