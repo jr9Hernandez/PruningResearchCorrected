2ae82ce0d24be5fd59bb51428c78a1304bdeccf1
Julian <Julian@DESKTOP-FCQ3307>
2017-03-18 02:46:24 -0300

    working

--- PruningMarioResearch/src/dk/itu/mario/engine/GenerationForm.java
+++ PruningMarioResearch/src/dk/itu/mario/engine/GenerationForm.java
@@ -92,11 +92,11 @@ public class GenerationForm {
 		
 	  	/*---------------------to Generate screens------------------------*/
 		
-			//int numElementsGlobalParamet=Integer.parseInt(args[0]);
+			int numElementsGlobalParamet=Integer.parseInt(args[0]);
 	    	//JFrame frame = new JFrame("Mario Experience Showcase");
-	    	MarioComponent mario = new MarioComponent(640, 480,true," ",5,4);
+	    	MarioComponent mario = new MarioComponent(640, 480,true," ",5,numElementsGlobalParamet);
 	    	System.out.println("");
-	    	System.out.print(4+" ");
+	    	System.out.print(numElementsGlobalParamet+" ");
 	    	/*
 	    	frame.setContentPane(mario);
 	    	frame.setResizable(false);
