d5d4b8138ebeab8500dcd25a1fee0e3b8da70b46
Julian <Julian@DESKTOP-FCQ3307>
2017-04-07 18:25:08 -0300

    Arreglé Conteo repetido de nodos!

--- PruningMarioResearch/src/beauty/BeautyCustomizedLevel.java
+++ PruningMarioResearch/src/beauty/BeautyCustomizedLevel.java
@@ -382,54 +382,54 @@ public class BeautyCustomizedLevel extends Level{
 	    	
 	        //3.8) B&B+heuristic +  Leviheuristic + Region and object Ordering
 	        //objElem.setFinalList(objElem.getFinalListNoOrder());
-	    	for(int i=0;i<5;i++)
+	    	for(int i=0;i<1;i++)
 	    	{
 	    		startTime = System.currentTimeMillis();
 	    		Beststates8=objGrapB8.DepthSearchCenterFramePruningRegionObjects(mediumStraight,height,numElements-numEnemies,numElements-numEnemies,states,objConstraints, objElem.getFinalList(),objElem,1,mediumStraight-2,floorTileHeight,0,0,numEnemies,random,globalControlSearch,8,typeSymmetry);
 	    		stopTime = System.currentTimeMillis();
 	    		time8 = stopTime - startTime;
-	    		sum8=sum8+time8;
+	    		//sum8=sum8+time8;
 	    	}
-	    	time8=sum8/5;
+	    	//time8=sum8/5;
 	        //System.out.println("Time B&B+heuristic + region ordering "+elapsedTime);
 	        
 	        //3.3) Brute-force search
 	        objElem.setFinalList(objElem.getFinalListNoOrder());
-	    	for(int i=0; i<5; i++)
+	    	for(int i=0; i<1; i++)
 	    	{
 	    		startTime = System.currentTimeMillis();
 	    		Beststates3=objGrapB3.DepthSearchCenterFrameNoPruningNoRegionsNoObjects(mediumStraight,height,numElements-numEnemies,numElements-numEnemies,states,objConstraints, objElem.getFinalList(),objElem,1,mediumStraight-2,floorTileHeight,0,0,numEnemies,random,globalControlSearch,8,typeSymmetry);
 	    		stopTime = System.currentTimeMillis();
 	    		time3 = stopTime - startTime;
-	    		sum3=sum3+time3;
+	    		//sum3=sum3+time3;
 	    	}
-	    	time3=sum3/5;
+	    	//time3=sum3/5;
 	        //System.out.println("Time Brute-force search "+elapsedTime);
 	    	
 	        //3.5) B&B+heuristic + region ordering + LeviHeuristic
 	        objElem.setFinalList(objElem.getFinalListNoOrder());
-	    	for(int i=0;i<5;i++)
+	    	for(int i=0;i<1;i++)
 	    	{
 	    		startTime = System.currentTimeMillis();
 	    		Beststates5=objGrapB5.DepthSearchCenterFramePruningRegion(mediumStraight,height,numElements-numEnemies,numElements-numEnemies,states,objConstraints, objElem.getFinalList(),objElem,1,mediumStraight-2,floorTileHeight,0,0,numEnemies,random,globalControlSearch,8,typeSymmetry);
 	    		stopTime = System.currentTimeMillis();
 	    		time5 = stopTime - startTime;
-	    		sum5=sum5+time5;
+	    		//sum5=sum5+time5;
 	    	}
-	    	time5=sum5/5;
+	    	//time5=sum5/5;
 	        //System.out.println("Time B&B+heuristic + region ordering "+elapsedTime);
 	        
 	        //3.6) B&B+heuristic +  LeviHeuristic
 	        objElem.setFinalList(objElem.getFinalListNoOrder());
-	    	for(int i=0;i<5;i++)
+	    	for(int i=0;i<1;i++)
 	    	{
 	    		startTime = System.currentTimeMillis();
 	    		Beststates6=objGrapB6.DepthSearchCenterFramePruningLeviHeuristic(mediumStraight,height,numElements-numEnemies,numElements-numEnemies,states,objConstraints, objElem.getFinalList(),objElem,1,mediumStraight-2,floorTileHeight,0,0,numEnemies,random,globalControlSearch,8,typeSymmetry);
 	    		stopTime = System.currentTimeMillis();
 	    		time6 = stopTime - startTime;
-	    		sum6=sum6+time6;
+	    		//sum6=sum6+time6;
 	    	}
-	    	time6=sum6/5;
+	    	//time6=sum6/5;
 	        //System.out.println("Time B&B+heuristic + region ordering "+elapsedTime);
 	        
 	        //3.7) B&B+heuristic +  oldoldheuristic
@@ -440,9 +440,9 @@ public class BeautyCustomizedLevel extends Level{
 	    		Beststates7=objGrapB7.DepthSearchCenterFramePruningOldOldHeuristic(mediumStraight,height,numElements-numEnemies,numElements-numEnemies,states,objConstraints, objElem.getFinalList(),objElem,1,mediumStraight-2,floorTileHeight,0,0,numEnemies,random,globalControlSearch,8,typeSymmetry);
 	    		stopTime = System.currentTimeMillis();
 	    		time7 = stopTime - startTime;
-	    		sum7=sum7+time7;
+	    		//sum7=sum7+time7;
 	    	}
-	    	time7=sum7/5;
+	    	//time7=sum7/5;
 	        //System.out.println("Time B&B+heuristic + region ordering "+elapsedTime);
 	    	
 
